de1089a59a5d52b0f03cb6422a2e50d5ce552a38
kashish shrivastav <en.shrivastavkashish@gmail.com>
2026-02-12 04:55:25 +0530

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function NotFound() {
   return (
@@ -12,7 +12,7 @@ export default function NotFound() {
         className="mb-8 h-10 w-fit"
       />
       <h2 className="header mb-6 max-w-[600px] text-center">Page Not Found</h2>
-      <p className="text-dark-700 mb-8">
+      <p className="mb-8 text-dark-700">
         The page you are looking for doesn&apos;t exist.
       </p>
       <Link href="/" className="text-green-500">
